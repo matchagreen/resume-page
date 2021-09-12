3915e7ffea427743492236df0a76e942f0dba2a1
rlsw35 <rlsw35@gmail.com>
2021-09-12 14:17:33 +0800

    deploying to github

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+function Footer() {
+  return (
+    <footer>
+      <div className="footer">
+        <div className="footerContent">
+          <p>This website is powered by ReactJS, HTML & CSS.</p>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
